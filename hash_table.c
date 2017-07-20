b29039bf4614a1e19320a22e6baea2debd3657c5
Ignat Loskutov <ignat.loskutov@gmail.com>
2017-07-20 19:54:40 +0300

    a

--- hash_table.c
+++ hash_table.c
@@ -11,12 +11,14 @@ struct hash_table {
 };
 
 static uint64_t to_power_of_2(uint64_t n) {
+	--n;
 	n |= n >> 1;
 	n |= n >> 2;
 	n |= n >> 4;
 	n |= n >> 8;
 	n |= n >> 16;
 	n |= n >> 32;
+	++n;
 	return n;
 }
 
